34f241af3c718919dde09ceb4e4041bf0d4f47a4
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-01-23 17:00:57 +0200

    Further adjustments to Search engine game

--- GGJ-FunnyGame/Assets/Scripts/Chris Scripts/ApplicationUsage.cs
+++ GGJ-FunnyGame/Assets/Scripts/Chris Scripts/ApplicationUsage.cs
@@ -6,6 +6,20 @@ public class ApplicationUsage : MonoBehaviour
 {
     public GameObject appWindow;
 
+    #region Search Engine Stuff
+    public GameObject searchWindow1;
+    public GameObject searchWindow2;
+    public GameObject searchWindow3;
+    public GameObject searchWindow4;
+    public GameObject searchWindow5;
+    public GameObject searchWindow6;
+    #endregion
+
+    private void Start()
+    {
+        SearchEngineWindows();
+    }
+
     public void OpenApp()
     {
         appWindow.SetActive(true);
@@ -14,5 +28,15 @@ public class ApplicationUsage : MonoBehaviour
     public void CloseApp()
     {
         appWindow.SetActive(false);
+    }
+
+    private void SearchEngineWindows()
+    {
+        searchWindow1.SetActive(true);
+        searchWindow2.SetActive(false);
+        searchWindow3.SetActive(false);
+        searchWindow4.SetActive(false);
+        searchWindow5.SetActive(false);
+        searchWindow6.SetActive(false);
     }
 }
