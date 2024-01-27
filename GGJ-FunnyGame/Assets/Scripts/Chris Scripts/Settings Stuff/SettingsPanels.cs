50e76a9eb399147e0ce4cce3fcdec194a4db9d0b
Simon Du Plooy <simondup9@gmail.com>
2024-01-27 09:49:34 +0200

    Volume Slider works now

--- GGJ-FunnyGame/Assets/Scripts/Chris Scripts/Settings Stuff/SettingsPanels.cs
+++ GGJ-FunnyGame/Assets/Scripts/Chris Scripts/Settings Stuff/SettingsPanels.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPanels : MonoBehaviour
 {
     public GameObject MainPanel;
     public GameObject panelOther1;
     public GameObject panelOther2;
+    public Slider volumeSlider;
+    public AudioSource musicSource;
+
+    public void Start()
+    {
+        if (gameObject.name == "Sound")
+        {
+            musicSource.gameObject.SetActive(true);
+        }
+    }
+
+    public void Update()
+    {
+        musicSource.volume = volumeSlider.value/100;
+    }
 
     public void ChangePanels()
     {
