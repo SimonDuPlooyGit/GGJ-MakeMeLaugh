fd7a4316048643c42ce4aef153a7d961b13af6d0
Simon Du Plooy <simondup9@gmail.com>
2024-01-28 07:45:27 +0200

    Fixed all inspector problems

---
+++ GGJ-FunnyGame/Assets/Scripts/Chris Scripts/Files Stuff/SendToPrinter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToPrinter : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    public void sendToPrint()
+    {
+        gameManager.GetComponent<GameManager>().imageSentToPrint = true;
+    }
+}
